ec41e48e168698340305053f09b332bb0e6029bf
Ubuntu <ubuntu@ip-172-31-39-105.eu-central-1.compute.internal>
2021-05-10 10:12:56 +0000

    initial commit

---
+++ sutil/CMakeFiles/sutil_sdk.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/Camera.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/Camera.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/OptiXMesh.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/OptiXMesh.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/sutil.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/sutil.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyobjloader/objLoader.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyobjloader/objLoader.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyplyloader/plyLoader.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyplyloader/plyLoader.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyplyloader/tinyply.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyplyloader/tinyply.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyxml/tinystr.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinystr.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyxml/tinyxml.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxml.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyxml/tinyxmlerror.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxmlerror.cpp.o"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil/tinyxml/tinyxmlparser.cpp" "/home/ubuntu/Documents/render/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/Downloads/NVIDIA-OptiX-SDK-5.1.0-linux64/include"
+  "/home/ubuntu/Documents/OptixRenderer/src/sutil"
+  "/home/ubuntu/Documents/OptixRenderer/src/optixRenderer/include"
+  "/home/ubuntu/Documents/OptixRenderer/src"
+  "/home/ubuntu/Downloads/NVIDIA-OptiX-SDK-5.1.0-linux64/include/optixu"
+  "."
+  "/usr/local/cuda/include"
+  "/home/ubuntu/Documents/OptixRenderer/src/device_include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
