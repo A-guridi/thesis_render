d956a83c8730a00da8c5a2ebe800425fa1c03fc6
ge93sut <ge93sut@mytum.de>
2021-05-25 19:18:48 +0200

    changes to areaLight.cu

--- src/optixRenderer/src/light/areaLight.cu
+++ src/optixRenderer/src/light/areaLight.cu
@@ -12,7 +12,7 @@ using namespace optix;
 
 rtDeclareVariable( float3, shading_normal, attribute shading_normal, ); 
 rtDeclareVariable( float3, geometric_normal, attribute geometric_normal, );
-rtDeclareVariable( float, t_hit, rtIntersectionDistance, );
+rtDeclareVariable( float, t_hit, rtIntersectionDistance, ); // with this the hitPoint can be calculated
 rtDeclareVariable(int, max_depth, , );
 
 rtDeclareVariable(optix::Ray, ray,   rtCurrentRay, );
@@ -27,6 +27,187 @@ rtBuffer<areaLight> areaLights;
 rtBuffer<float> areaLightCDF;
 rtBuffer<float> areaLightPDF;
 
+// HERE STARTS THE CUSTOM IMPLEMENTATION OF THE SHADER
+// the implementation is based on the ray tracing algorithm made in Falcor
+#define TMIN 0.001
+#define TMAX 10000.0
+
+#define M_PI     3.14159265358979323846
+#define M_PI2    6.28318530717958647692
+#define M_INV_PI 0.3183098861837906715
+
+#define COLOR_BLACK  float3(0.0, 0.0, 0.0)
+#define COLOR_RED    float3(1.0, 0.0, 0.0)
+#define COLOR_GREEN  float3(0.0, 1.0, 0.0)
+#define COLOR_BLUE   float3(0.0, 0.0, 1.0)
+#define COLOR_CYAN   float3(0.0, 1.0, 1.0)
+#define COLOR_YELLOW float3(1.0, 1.0, 0.0)
+
+#define REFL_MISS_COLOR COLOR_BLACK
+
+//**************************************************************************************************
+// We dont need constant buffers as they are the rtVaariables
+//**************************************************************************************************
+
+//**************************************************************************************************
+// Data structures and initializer functions
+//**************************************************************************************************
+
+struct MuellerData
+{
+    // red, green, and blue Mueller matrices
+    float4x4 mmR;
+    float4x4 mmG;
+    float4x4 mmB;
+};
+
+struct StokesLight
+{
+    // red, green, and blue Stokes vectors
+    float4 svR;
+    float4 svG;
+    float4 svB;
+
+    // local coordinate system's x-axis unit vector
+    float3 referenceX;
+};
+
+StokesLight initStokes()
+{
+    StokesLight sl;
+    sl.svR = float4(0.0, 0.0, 0.0, 0.0);
+    sl.svG = float4(0.0, 0.0, 0.0, 0.0);
+    sl.svB = float4(0.0, 0.0, 0.0, 0.0);
+    sl.referenceX = float3(1.0, 0.0, 0.0);
+
+    return sl;
+}
+
+StokesLight unPolarizedLight(float3 color)
+{
+    StokesLight sl = initStokes();
+    sl.svR.x = color.r;
+    sl.svG.x = color.g;
+    sl.svB.x = color.b;
+    return sl;
+}
+
+float3 stokesToColor(StokesLight sl) {
+    return saturate(float3(sl.svR.x, sl.svG.x, sl.svB.x));
+}
+//**************************************************************************************************
+// Operator macros for StokesLight and MuellerData
+//**************************************************************************************************
+
+// operator += for StokesLight and unpolarized float3 color
+// unpolarized light can be added directly, no need to first create a Stokes vector for it
+#define SL_ADD_EQ_UNPOL(sl, c) sl.svR.x += c.r; \
+                               sl.svG.x += c.g; \
+                               sl.svB.x += c.b;
+
+// operator += for two already aligned StokesLight parameters
+#define SL_ADD_EQ_POL(sl_a, sl_b) sl_a.svR += sl_b.svR; \
+                                  sl_a.svG += sl_b.svG; \
+                                  sl_a.svB += sl_b.svB;
+
+// operator *= for StokesLight and MuellerData
+#define SL_MUL_EQ_MD(sl, md) sl.svR = mul(sl.svR, md.mmR); \
+                             sl.svG = mul(sl.svG, md.mmG); \
+                             sl.svB = mul(sl.svB, md.mmB);
+
+
+// operator *= for MuellerData and a scalar
+#define MD_MUL_EQ_SCALAR(md, s) md.mmR *= s; md.mmG *= s; md.mmB *= s;
+
+//**************************************************************************************************
+// Ray payload
+//**************************************************************************************************
+
+
+void initPayload()
+{
+    // adapted function to use the prd_radiance instead of creating a new ray payload
+    prd_radiance.lightData=initStokes();
+}
+//**************************************************************************************************
+// StokesLight functions
+//**************************************************************************************************
+
+// Get the normalized reference x vector from normalized y and z vectors
+float3 computeX(float3 y, float3 z)
+{
+    return normalize(cross(y, z));
+}
+
+/** Rotate the reference frame of a Stokes vector
+  c2p: cos(2phi)
+  s2p: sin(2phi)
+  inout param substitued by reference &
+*/
+void rotateStokes(float4 &S, float c2p, float s2p)
+{
+    float old_y = S.y;
+    float old_z = S.z;
+
+    S.y =  c2p*old_y + s2p*old_z;
+    S.z = -s2p*old_y + c2p*old_z;
+}
+
+/** Rotate reference frame
+  The light's direction vector dir is needed to rotate the reference X around
+*/
+void rotateReferenceFrame(StokesLight &light, float3 newX, float3 dir)
+{
+    float dotX = dot(light.referenceX, newX);
+    float detX = dot(dir, cross(light.referenceX, newX));
+    float phi = atan2(detX, dotX);
+
+    float c2p = cos(2*phi);
+    float s2p = sin(2*phi);
+
+    rotateStokes(light.svR, c2p, s2p);
+    rotateStokes(light.svG, c2p, s2p);
+    rotateStokes(light.svB, c2p, s2p);
+    light.referenceX = newX;
+}
+
+/** a+=b operator for StokesLight
+  Rotates b's reference frame before addition if needed.
+*/
+void slAddEquals(StokesLight &a, StokesLight b, float3 dir)
+{
+    // Make sure b's reference frame matches a's before adding them
+    rotateReferenceFrame(b, a.referenceX, dir);
+    SL_ADD_EQ_POL(a, b);
+}
+
+/** Applies the polarizing filter to a Stokes vector s
+*/
+void polarizeStokes(float4 &s)
+{
+    uniform float a = filterCos2A;
+    uniform float b = filterSin2A;
+    float3 oldXYZ = s.xyz;
+
+    s.x = dot(oldXYZ, float3(1.0,   a,   b));
+    s.y = dot(oldXYZ, float3(  a, a*a, a*b));
+    s.z = dot(oldXYZ, float3(  b, a*b, b*b));
+    s.w = 0.0;
+}
+
+/** Applies a horizontal polarizing filter that's been rotated clockwise by angle A
+  Note: also doubles the intensity to compensate for the filter on average blocking half of the
+        incoming light.
+*/
+void applyPolarizingFilter(StokesLight &l)
+{
+    polarizeStokes(l.svR);
+    polarizeStokes(l.svG);
+    polarizeStokes(l.svB);
+}
+/**
+ * RENDER PARTS
+ * */
 RT_CALLABLE_PROGRAM void sampleAreaLight(unsigned int& seed, float3& radiance, float3& position, float3& normal, float& pdfAreaLight){
     float randf = rnd(seed);
 
